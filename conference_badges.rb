29268dd440a2900d143dd914a27e2e7f0cfd39f7
Kyle Houghton <kylebenz@hotmail.com>
2018-12-22 23:48:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+
+require 'pry'
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  output_array = []
+  attendees.each {|name| output_array.push(badge_maker(name))}
+  output_array
+end
+
+def assign_rooms(attendees)
+  
+end
+
+def printer(attendees)
+end
+
+binding.pry
