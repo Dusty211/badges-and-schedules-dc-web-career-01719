2355810d7c021a2189a326b259ae3cc271dec44a
Kyle Houghton <kylebenz@hotmail.com>
2018-12-23 02:57:09 +0000

    All tests pass.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  output_array = Array.new
+  attendees.each {|name| output_array.push(badge_maker(name))}
+  output_array
+end
+
+def assign_rooms(attendees)
+  room_assignments = Array.new
+  attendees.each_with_index {|name, index| room_assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")}
+  room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|badge_greeting| puts badge_greeting}
+  assign_rooms(attendees).each {|room_greeting| puts room_greeting}
+end
+
